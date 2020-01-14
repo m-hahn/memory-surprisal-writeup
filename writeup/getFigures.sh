2a455cbc875cca4dc5d2d4073e29872e9733590a
Michael Hahn <mhahn29@gmail.com>
2020-01-14 09:13:47 -0800

    initial

---
+++ writeup/getFigures.sh
@@ -0,0 +1,11 @@
+cp ~/memory-surprisal/code/toy/figures/decay.pdf figures/
+cp ~/memory-surprisal/code/toy/figures/memory.pdf figures/
+cp ~/memory-surprisal/code/toy/figures/add-surp.pdf figures/
+cp ~/memory-surprisal/code/toy/figures/lower-mem.pdf figures/
+cp ~/memory-surprisal/code/toy/figures/listener-tradeoff.pdf figures/
+cp ~/memory-surprisal/code/toy/figures/toy-mis.pdf figures/
+cp ~/memory-surprisal/code/toy/figures/toy-t-mis.pdf figures/
+cp ~/memory-surprisal/code/toy/figures/toy-mem-surp.pdf figures/
+cp ~/memory-surprisal/code/visualize_neural/figures/surprisal-branching-entropy-REAL.pdf figures/
+cp ~/memory-surprisal/code/visualize_neural/figures/full-GROUND-listener-surprisal-memory-HIST_z_byMem_onlyWordForms_boundedVocab.pdf figures/
+
