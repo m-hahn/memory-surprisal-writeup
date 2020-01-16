30defd24a3ef17221aef2de3d5a1ff7dc64b06b4
Michael Hahn <mhahn29@gmail.com>
2020-01-16 13:49:11 -0800

    code

--- writeup/getFigures.sh
+++ writeup/getFigures.sh
@@ -8,4 +8,4 @@ cp ~/memory-surprisal/code/toy/figures/toy-t-mis.pdf figures/
 cp ~/memory-surprisal/code/toy/figures/toy-mem-surp.pdf figures/
 cp ~/memory-surprisal/code/visualize_neural/figures/surprisal-branching-entropy-REAL.pdf figures/
 cp ~/memory-surprisal/code/visualize_neural/figures/full-GROUND-listener-surprisal-memory-HIST_z_byMem_onlyWordForms_boundedVocab.pdf figures/
-
+cp ~/memory-surprisal/code/order-freedom/infostruc/analysis/visualize_neural/figures/Czech-PDT-listener-surprisal-memory-MEDIANS_onlyWordForms_boundedVocab.pdf figures/
