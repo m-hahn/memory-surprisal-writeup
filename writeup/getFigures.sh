8d93582f4d2041753cb33961b70994fd6c6f9f21
Michael Hahn <mhahn29@gmail.com>
2020-02-05 13:33:56 -0800

    AUC table

--- writeup/getFigures.sh
+++ writeup/getFigures.sh
@@ -10,4 +10,5 @@ cp ~/memory-surprisal/code/visualize_neural/figures/surprisal-branching-entropy-
 cp ~/memory-surprisal/code/visualize_neural/figures/full-GROUND-listener-surprisal-memory-HIST_z_byMem_onlyWordForms_boundedVocab.pdf figures/
 cp ~/memory-surprisal/code/order-freedom/infostruc/analysis/visualize_neural/figures/Czech-PDT-listener-surprisal-memory-MEDIANS_onlyWordForms_boundedVocab.pdf figures/
 cp ~/memory-surprisal/writeup/quantiles-table.pdf .
+cp ~/memory-surprisal/writeup/auc-table.pdf .
 
