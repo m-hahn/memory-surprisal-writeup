606c769b9049adf43265ef853a7d326a2b182da0
Michael Hahn <mhahn29@gmail.com>
2020-05-10 19:21:12 +0800

    code

--- writeup/getFigures.sh
+++ writeup/getFigures.sh
@@ -6,8 +6,8 @@ cp ~/memory-surprisal/code/study1/figures/listener-tradeoff.pdf figures/
 cp ~/memory-surprisal/code/study1/figures/toy-mis.pdf figures/
 cp ~/memory-surprisal/code/study1/figures/toy-t-mis.pdf figures/
 cp ~/memory-surprisal/code/study1/figures/toy-mem-surp.pdf figures/
-cp ~/memory-surprisal/code/visualize_neural/figures/surprisal-branching-entropy-REAL.pdf figures/
-cp ~/memory-surprisal/code/visualize_neural/figures/full-GROUND-listener-surprisal-memory-HIST_z_byMem_onlyWordForms_boundedVocab.pdf figures/
+cp ~/memory-surprisal/code/analysis/visualize_neural/figures/surprisal-branching-entropy-REAL.pdf figures/
+cp ~/memory-surprisal/code/analysis/visualize_neural/figures/full-GROUND-listener-surprisal-memory-HIST_z_byMem_onlyWordForms_boundedVocab.pdf figures/
 cp ~/memory-surprisal/code/order-freedom/infostruc/analysis/visualize_neural/figures/Czech-PDT-listener-surprisal-memory-MEDIANS_onlyWordForms_boundedVocab.pdf figures/
 cp ~/memory-surprisal/writeup/quantiles-table.pdf .
 cp ~/memory-surprisal/writeup/auc-table.pdf .
