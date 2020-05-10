58523974bec9edcb9d1b470c6bdbe6ba2e3c887d
Michael Hahn <mhahn29@gmail.com>
2020-05-10 14:28:13 +0800

    code

--- writeup/getFigures.sh
+++ writeup/getFigures.sh
@@ -1,11 +1,11 @@
-cp ~/memory-surprisal/code/experiment1/figures/decay.pdf figures/
-cp ~/memory-surprisal/code/experiment1/figures/memory.pdf figures/
-cp ~/memory-surprisal/code/experiment1/figures/add-surp.pdf figures/
-cp ~/memory-surprisal/code/experiment1/figures/lower-mem.pdf figures/
-cp ~/memory-surprisal/code/experiment1/figures/listener-tradeoff.pdf figures/
-cp ~/memory-surprisal/code/experiment1/figures/toy-mis.pdf figures/
-cp ~/memory-surprisal/code/experiment1/figures/toy-t-mis.pdf figures/
-cp ~/memory-surprisal/code/experiment1/figures/toy-mem-surp.pdf figures/
+cp ~/memory-surprisal/code/study1/figures/decay.pdf figures/
+cp ~/memory-surprisal/code/study1/figures/memory.pdf figures/
+cp ~/memory-surprisal/code/study1/figures/add-surp.pdf figures/
+cp ~/memory-surprisal/code/study1/figures/lower-mem.pdf figures/
+cp ~/memory-surprisal/code/study1/figures/listener-tradeoff.pdf figures/
+cp ~/memory-surprisal/code/study1/figures/toy-mis.pdf figures/
+cp ~/memory-surprisal/code/study1/figures/toy-t-mis.pdf figures/
+cp ~/memory-surprisal/code/study1/figures/toy-mem-surp.pdf figures/
 cp ~/memory-surprisal/code/visualize_neural/figures/surprisal-branching-entropy-REAL.pdf figures/
 cp ~/memory-surprisal/code/visualize_neural/figures/full-GROUND-listener-surprisal-memory-HIST_z_byMem_onlyWordForms_boundedVocab.pdf figures/
 cp ~/memory-surprisal/code/order-freedom/infostruc/analysis/visualize_neural/figures/Czech-PDT-listener-surprisal-memory-MEDIANS_onlyWordForms_boundedVocab.pdf figures/
@@ -15,8 +15,8 @@ cp ~/memory-surprisal/writeup/auc-table_MLE.pdf .
 cp ~/memory-surprisal/writeup/it-table.pdf .
 cp ~/memory-surprisal/writeup/results-table.pdf .
 cp ~/memory-surprisal/writeup/results-table-mle.pdf .
-cp ~/memory-surprisal/code/ngram-control/create_models_ngrams/morph/Japanese/tradeoffs/figures/* figures/
-cp ~/memory-surprisal/code/ngram-control/create_models_ngrams/morph/Sesotho/tradeoffs/figures/* figures/
+cp ~/memory-surprisal/code/study3/Japanese/tradeoffs/figures/* figures/
+cp ~/memory-surprisal/code/study3/Sesotho/tradeoffs/figures/* figures/
 cp ~/memory-surprisal/code/analysis/visualize_neural/figures/surprisal-branching-entropy-REAL*.pdf figures/
 cp ~/memory-surprisal/code/order-freedom/infostruc/analysis/medians/auc/figures/* figures/
 
